5654168710d4db8c79cef7b646d0b99bceccf081
Sandrico Provo <Sprovo@outlook.com>
2023-04-29 18:08:40 -0300

    feat(cli): converts project to cli tool.

--- main.go
+++ main.go
@@ -1,41 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"sandricoprovo/denoken/builders"
-	"sandricoprovo/denoken/structs"
-	"sandricoprovo/denoken/utils"
+	"sandricoprovo/denoken/cmd"
 )
 
 func main() {
-	// TODO:
-	// - Convert type scale to use rems optionally
-
-	configPaths := structs.ConfigPaths{
-		File:  "denoken.config",
-		Paths: []string{".", "./config/"},
-	}
-
-	// Loads config file settings
-	config, configErr := utils.LoadConfig(configPaths)
-	if configErr != nil {
-		fmt.Println(configErr)
-	}
-	// Builds the type scale struct to be used for generating this block of css
-	typeScale, scaleGeneratorErr := builders.BuildTypeScale(config.TypeScale)
-	if scaleGeneratorErr != nil {
-		log.Fatal(scaleGeneratorErr)
-	}
-
-	cssBlocks := structs.CSSBlocks{
-		TypeScale: typeScale,
-	}
-
-	createFileErr := utils.CreateGlobalCssFile(cssBlocks, config.Path)
-	if createFileErr != nil {
-		log.Fatal("🛑 There was an issue generating the css file. Please try again.")
-	}
-
-	fmt.Println("⭐ Successfully created your css file at /" + config.Path)
+	cmd.Execute()
 }
